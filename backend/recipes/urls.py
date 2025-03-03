1134ba73e639e5452ef74afad8f5e1235a82f271
Анна Иванова <ivalex.anna@gmail.com>
2025-03-03 21:35:08 +0300

    3

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from api import views
+from api.views import RecipeViewSet
 
 urlpatterns = [
-    path("s/<slug:url_slug>/", views.url_redirect, name="url_redirect"),
+    path("s/<slug:url_slug>/", RecipeViewSet, name="url_redirect"),
 ]
