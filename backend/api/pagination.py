a50fbba17b5dacdd1c10b73c56f3873578d34116
Анна Иванова <ivalex.anna@gmail.com>
2025-03-05 22:30:09 +0300

    pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,8 +1,13 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
+
+from django.conf import settings
+
+PAGE_SIZE = settings.REST_FRAMEWORK["PAGE_SIZE"]
 
 
-class CustomPagination(LimitOffsetPagination):
+class CustomPagination(PageNumberPagination):
     """Кастомная пагинация."""
 
-    default_limit = 6
-    max_limit = 100
+    page_size = PAGE_SIZE
+    page_size_query_param = "limit"
+    max_page_size = PAGE_SIZE
